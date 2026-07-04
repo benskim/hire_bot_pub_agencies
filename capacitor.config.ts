aedd954e11256fb71f9e6cf350edccbe164d46de
ben.sunki.kim <benskim915@gmail.com>
2026-07-04 12:01:53 +0000

    Add Android build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pubjob.app',
+  appName: 'pub-agencies-job',
+  webDir: 'dist'
+};
+
+export default config;
