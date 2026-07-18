84ab48c61d9910d86b408abeb8a27a7b3e668521
ben.sunki.kim <benskim915@gmail.com>
2026-07-18 11:10:59 +0000

    init : capacitor

---
+++ android/app/src/main/java/com/hireNoti/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.hireNoti.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hireNoti.app',
+  appName: 'hire_noti',
+  webDir: 'dist'
+};
+
+export default config;
